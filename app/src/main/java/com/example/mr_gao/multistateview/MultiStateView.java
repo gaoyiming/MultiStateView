7f514f3a9ad914ae55b18da6d6817a8f1662275d
Mr_gao <787080436@qq.com>
2016-10-18 22:18:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mr_gao/multistateview/MultiStateView.java
@@ -0,0 +1,29 @@
+package com.example.mr_gao.multistateview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * @ClassName: MultiStateView
+ * @Description: ${TODO}
+ * @author: Mr_gao
+ * @date: 2016/10/18 22:13
+ */
+public class MultiStateView extends FrameLayout {
+    public MultiStateView(Context context) {
+        super(context);
+    }
+
+    public MultiStateView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
