4d3735401bc46db56c7f72a3e421aa35e266851d
Mr_gao <787080436@qq.com>
2016-10-18 22:44:57 +0800

    基础书写

--- app/src/main/java/com/example/mr_gao/multistateview/MultiStateView.java
+++ app/src/main/java/com/example/mr_gao/multistateview/MultiStateView.java
@@ -1,7 +1,9 @@
 package com.example.mr_gao.multistateview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
 /**
@@ -11,19 +13,46 @@ import android.widget.FrameLayout;
  * @date: 2016/10/18 22:13
  */
 public class MultiStateView extends FrameLayout {
+    private  Context context;
+
     public MultiStateView(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public MultiStateView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
     }
 
     public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context=context;
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MultiStateView, 0, 0);
+        int empty_layout = typedArray.getResourceId(R.styleable.MultiStateView_emptylayout, R.layout.empty_layout);
+        int loading_layout = typedArray.getResourceId(R.styleable.MultiStateView_loadinglayout, R.layout.loading_layout);
+        int nonet_layout = typedArray.getResourceId(R.styleable.MultiStateView_nonoetlayout, R.layout.nonet_layout);
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        layoutInflater.inflate(empty_layout,this,true);
+        layoutInflater.inflate(loading_layout,this,true);
+        layoutInflater.inflate(nonet_layout,this,true);
     }
+
 
     public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+    public void showEmptyView(){
+
+    }
+    public void showLoadingView(){
+
+    }
+    public void showNoNetView(){
+
+    }
+    public void showContentView(){
+
+    }
+    public void GoneAllViews(){
+
     }
 }
