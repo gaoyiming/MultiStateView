8f63814486446a50a72359404f0c6a1e01d6b823
Mr_gao <787080436@qq.com>
2016-10-19 21:51:44 +0800

    添加几个布局

--- app/src/main/java/com/example/mr_gao/multistateview/MainActivity.java
+++ app/src/main/java/com/example/mr_gao/multistateview/MainActivity.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        MultiStateView multyview = (MultiStateView) findViewById(R.id.multyview);
+        multyview.showLoadingView();
+
+      //  multyview.showContentView();
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
--- app/src/main/java/com/example/mr_gao/multistateview/MultiStateView.java
+++ app/src/main/java/com/example/mr_gao/multistateview/MultiStateView.java
@@ -2,8 +2,11 @@ package com.example.mr_gao.multistateview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 /**
@@ -13,46 +16,91 @@ import android.widget.FrameLayout;
  * @date: 2016/10/18 22:13
  */
 public class MultiStateView extends FrameLayout {
-    private  Context context;
+    private static final int EMPTYVIEW = 1;
+    private static final int LOADINGVIEW = 2;
+    private static final int NONETVIEW = 3;
+    private static final int ALLVIEW = 4;
+    private Context context;
+    private View emptyView;
+    private View loadingView;
+    private View noNetView;
+    private LayoutParams layoutParams;
 
     public MultiStateView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public MultiStateView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.context=context;
+        this.context = context;
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MultiStateView, 0, 0);
         int empty_layout = typedArray.getResourceId(R.styleable.MultiStateView_emptylayout, R.layout.empty_layout);
         int loading_layout = typedArray.getResourceId(R.styleable.MultiStateView_loadinglayout, R.layout.loading_layout);
         int nonet_layout = typedArray.getResourceId(R.styleable.MultiStateView_nonoetlayout, R.layout.nonet_layout);
+        layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         LayoutInflater layoutInflater = LayoutInflater.from(context);
-        layoutInflater.inflate(empty_layout,this,true);
-        layoutInflater.inflate(loading_layout,this,true);
-        layoutInflater.inflate(nonet_layout,this,true);
+        emptyView = View.inflate(context, empty_layout,null);
+        loadingView = View.inflate(context, loading_layout,null);
+        noNetView = View.inflate(context, nonet_layout,null);
+        addView(emptyView,layoutParams);
+        addView(loadingView,layoutParams);
+        addView(noNetView,layoutParams);
+//        emptyView = layoutInflater.inflate(empty_layout, this, true);
+//        loadingView = layoutInflater.inflate(loading_layout, this, true);
+//        noNetView = layoutInflater.inflate(nonet_layout, this, true);
     }
 
 
     public MultiStateView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
-    public void showEmptyView(){
 
+    public void showEmptyView() {
+        GoneOtherViews(EMPTYVIEW);
     }
-    public void showLoadingView(){
 
+    public void showLoadingView() {
+        GoneOtherViews(LOADINGVIEW);
     }
-    public void showNoNetView(){
 
+    public void showNoNetView() {
+        GoneOtherViews(NONETVIEW);
     }
-    public void showContentView(){
 
+    public void showContentView(View view) {
+        GoneOtherViews(ALLVIEW);
+      //  addView(view, layoutParams);
     }
-    public void GoneAllViews(){
+
+    public void GoneOtherViews(int ViewTag) {
+        switch (ViewTag) {
+            case EMPTYVIEW:
+                emptyView.setVisibility(VISIBLE);
+                loadingView.setVisibility(INVISIBLE);
+                noNetView.setVisibility(INVISIBLE);
+                break;
+            case LOADINGVIEW:
+                emptyView.setVisibility(INVISIBLE);
+                loadingView.setVisibility(VISIBLE);
+                noNetView.setVisibility(INVISIBLE);
+                break;
+            case NONETVIEW:
+                emptyView.setVisibility(INVISIBLE);
+                loadingView.setVisibility(INVISIBLE);
+                noNetView.setVisibility(VISIBLE);
+                break;
+            case ALLVIEW:
+                emptyView.setVisibility(INVISIBLE);
+                loadingView.setVisibility(INVISIBLE);
+                noNetView.setVisibility(INVISIBLE);
+                break;
+
+        }
+
 
     }
 }
